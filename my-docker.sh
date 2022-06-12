6d010616cf62d793b6cbd780a5925ecf24f18a62
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-12 01:34:44 -0400

    .

--- my-docker.sh
+++ my-docker.sh
@@ -1,3 +1,24 @@
 wget "https://raw.githubusercontent.com/Personal-Linux-Scripts/Ubuntu-Scripts/main/docker.sh"
 chmod +x ./docker.sh
 ./docker.sh
+
+rm -f ./docker.sh ./docker-compose.yml
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.9.3
+
+wget "https://raw.githubusercontent.com/Personal-Linux-Scripts/Docker-Containers/main/deemix/docker-compose.yml"
+sudo docker-compose up -d
+rm -f ./docker-compose.yml
+
+wget "https://raw.githubusercontent.com/Personal-Linux-Scripts/Docker-Containers/main/heimdall/docker-compose.yml"
+sudo docker-compose up -d
+rm -f ./docker-compose.yml
+
+wget "https://raw.githubusercontent.com/Personal-Linux-Scripts/Docker-Containers/main/jellyfin/docker-compose.yml"
+sudo docker-compose up -d
+rm -f ./docker-compose.yml
+
+wget "https://raw.githubusercontent.com/Personal-Linux-Scripts/Docker-Containers/main/navidrome/docker-compose.yml"
+sudo docker-compose up -d
+rm -f ./docker-compose.yml
